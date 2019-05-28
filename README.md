5f76c9c42af1d00fdce7bf99264d56a4f5596d2b
Magic Len <len@magiclen.org>
2019-05-29 05:09:42 +0800

    release 0.1

--- README.md
+++ README.md
@@ -1,25 +1,38 @@
 Favicon Generator
 ====================
 
+[![Build Status](https://travis-ci.org/magiclen/favicon-generator.svg?branch=master)](https://travis-ci.org/magiclen/favicon-generator)
+
 It helps you generate favicons with different formats and sizes.
 
 ## Help
 
 ```
 EXAMPLES:
-  favicon-generator -i /path/to/image -o /path/to/folder     # Use /path/to/image to generate favicons into
-/path/to/folder
+  favicon-generator /path/to/image /path/to/folder     # Use /path/to/image to generate favicons into /path/to/folder
 
 USAGE:
-    favicon-generator --input <INPUT_PATH> --output <OUTPUT_PATH>
+    favicon-generator [FLAGS] [OPTIONS] <INPUT_PATH> <OUTPUT_PATH>
 
 FLAGS:
-    -h, --help       Prints help information
-    -V, --version    Prints version information
+    -y, --overwrite    Overwrites exiting files without asking
+    -h, --help         Prints help information
+    -V, --version      Prints version information
 
 OPTIONS:
-    -i, --input <INPUT_PATH>      Assigns an image for generating favicons. It should be a path of a file.
-    -o, --output <OUTPUT_PATH>    Assigns a destination of your generated files. It should be a path of a directory.
+        --potrace-path <POTRACE_PATH>             Specifies the path of your potrace executable binary file [default: potrace]
+        --path-prefix <PATH_PREFIX>               Specifies the path prefix of your favicon files [default: /]
+    -t, --threshold <FLOATING_VALUE>              The black/white cutoff in input file [default: 0.5]
+        --app-name <NAME>                         Assigns a name for your web app
+        --app-short-name <NAME>                   Assigns a short name for your web app
+        --background-color <HEX_COLOR>            Forces to assign a background color for all devices [aliases: background]
+        --android-background-color <HEX_COLOR>    Assigns a background color for Android devices [default: #FFFFFF]  [aliases: android-background]
+        --ios-background-color <HEX_COLOR>        Assigns a background color for iOS devices [default: #FFFFFF]  [aliases: ios-background]
+        --windows-background-color <HEX_COLOR>    Assigns a background color for Windows devices [default: #FFFFFF]  [aliases: windows-background]
+
+ARGS:
+    <INPUT_PATH>     Assigns an image for generating favicons. It should be a path of a file
+    <OUTPUT_PATH>    Assigns a destination of your generated files. It should be a path of a directory
 ```
 
 ## License
--- src/lib.rs
+++ src/lib.rs
@@ -8,18 +8,17 @@ extern crate scanner_rust;
 #[macro_use]
 extern crate validators;
 extern crate subprocess;
-extern crate threadpool;
 extern crate num_cpus;
 #[macro_use]
 extern crate serde_json;
 extern crate tera;
+#[macro_use]
+extern crate lazy_static;
 
 use std::env;
 use std::path::{Path, PathBuf};
 use std::fs;
 use std::io::{self, ErrorKind, Write};
-use std::thread;
-use std::sync::{Arc, Mutex, mpsc};
 
 use terminal_size::{Width, terminal_size};
 use clap::{App, Arg};
@@ -29,6 +28,7 @@ use image_convert::{ColorName, ImageResource, PGMConfig, PNGConfig, ICOConfig, i
 use scanner_rust::{Scanner, ScannerError};
 
 use validators::boolean::Boolean;
+use validators::regex::Regex;
 
 use subprocess::{Exec, ExitStatus, PopenError, NullFile};
 
@@ -54,8 +54,14 @@ const FILE_PNG_IOS_BACKGROUND: &str = "favicon-180-i.png";
 const ICO_SIZE: [u16; 3] = [48, 32, 16];
 const PNG_SIZE: [u16; 7] = [512, 310, 192, 150, 70, 32, 16];
 
+lazy_static! {
+    static ref RE_HEX_COLOR: Regex = {
+        Regex::new("^#[0-f0-F]{6}$").unwrap()
+    };
+}
+
 validated_customized_ranged_number!(pub Threshold, f64, 0f64, 1.0f64);
-validated_customized_regex_string!(pub HexColor, "^#[0-f0-F]{6}$");
+validated_customized_regex_string!(pub HexColor, ref RE_HEX_COLOR);
 
 #[derive(Debug)]
 pub struct ExePaths {
@@ -86,7 +92,6 @@ pub struct Config {
     pub path_prefix: PathBuf,
     pub overwrite: bool,
     pub threshold: Threshold,
-    pub single_thread: bool,
     pub app_name: String,
     pub app_short_name: String,
     pub android_background_color: HexColor,
@@ -159,50 +164,44 @@ impl Config {
                 .default_value(DEFAULT_THRESHOLD)
                 .display_order(3)
             )
-            .arg(Arg::with_name("SINGLE_THREAD")
-                .long("single-thread")
-                .short("s")
-                .help("Uses only one thread")
-                .display_order(4)
-            )
             .arg(Arg::with_name("APP_NAME").value_name("NAME")
                 .long("app-name")
                 .help("Assigns a name for your web app")
                 .takes_value(true)
-                .display_order(5)
+                .display_order(4)
             )
             .arg(Arg::with_name("APP_SHORT_NAME").value_name("NAME")
                 .long("app-short-name")
                 .help("Assigns a short name for your web app")
                 .takes_value(true)
-                .display_order(6)
+                .display_order(5)
             )
             .arg(Arg::with_name("BACKGROUND_COLOR").value_name("HEX_COLOR")
                 .long("background-color").visible_alias("background")
                 .help("Forces to assign a background color for all devices")
                 .takes_value(true)
-                .display_order(7)
+                .display_order(6)
             )
             .arg(Arg::with_name("ANDROID_BACKGROUND_COLOR").value_name("HEX_COLOR")
                 .long("android-background-color").visible_alias("android-background")
                 .help("Assigns a background color for Android devices")
                 .takes_value(true)
                 .default_value(DEFAULT_BACKGROUND_COLOR)
-                .display_order(8)
+                .display_order(7)
             )
             .arg(Arg::with_name("IOS_BACKGROUND_COLOR").value_name("HEX_COLOR")
                 .long("ios-background-color").visible_alias("ios-background")
                 .help("Assigns a background color for iOS devices")
                 .takes_value(true)
                 .default_value(DEFAULT_BACKGROUND_COLOR)
-                .display_order(9)
+                .display_order(8)
             )
             .arg(Arg::with_name("WINDOWS_BACKGROUND_COLOR").value_name("HEX_COLOR")
                 .long("windows-background-color").visible_alias("windows-background")
                 .help("Assigns a background color for Windows devices")
                 .takes_value(true)
                 .default_value(DEFAULT_BACKGROUND_COLOR)
-                .display_order(10)
+                .display_order(9)
             )
             .after_help("Enjoy it! https://magiclen.org")
             .get_matches();
@@ -231,7 +230,6 @@ impl Config {
             path_prefix: matches.value_of("PATH_PREFIX").unwrap().into(),
             overwrite: matches.is_present("OVERWRITE"),
             threshold: Threshold::from_str(matches.value_of("THRESHOLD").unwrap()).map_err(|err| err.to_string())?,
-            single_thread: matches.is_present("SINGLE_THREAD"),
             app_name: matches.value_of("APP_NAME").map(|s| s.into()).unwrap_or(String::new()),
             app_short_name: matches.value_of("APP_SHORT_NAME").map(|s| s.into()).unwrap_or(String::new()),
             android_background_color,
@@ -415,28 +413,26 @@ pub fn run(config: Config) -> Result<i32, String> {
 
     let mut tera = Tera::default();
 
-    tera.add_raw_template("browser_config", include_str!("resources/browser-config.xml")).map_err(|err| err.to_string())?;
+    tera.add_raw_template("browser-config", include_str!("resources/browser-config.xml")).map_err(|err| err.to_string())?;
 
+    let (_ident, mw) = {
+        let input = ImageResource::Path(input_str.to_string());
 
-    if config.single_thread {
-        let (_ident, mw) = {
-            let input = ImageResource::Path(input_str.to_string());
+        let mut output = Some(None);
 
-            let mut output = Some(None);
+        let ident = identify(&mut output, &input).map_err(|err| err.to_string())?;
 
-            let ident = identify(&mut output, &input).map_err(|err| err.to_string())?;
+        (ident, output.unwrap().unwrap())
+    };
 
-            (ident, output.unwrap().unwrap())
-        };
+    let mw_input = ImageResource::MagickWand(mw);
 
-        let mw_input = ImageResource::MagickWand(mw);
+    {
+        // web_app_manifest
+        let src_192 = format!("{}favicon-192.png", path_prefix);
+        let src_512 = format!("{}favicon-512.png", path_prefix);
 
-        {
-            // web_app_manifest
-            let src_192 = format!("{}favicon-192.png", path_prefix);
-            let src_512 = format!("{}favicon-512.png", path_prefix);
-
-            let content = json!(
+        let content = json!(
                 {
                     "name": config.app_name,
                     "short_name": config.app_short_name,
@@ -452,194 +448,113 @@ pub fn run(config: Config) -> Result<i32, String> {
                             "type": "image/png"
                         }
                     ],
-                    "theme_color": config.android_background_color.clone(),
+                    "theme_color": &config.android_background_color,
                     "background_color": config.android_background_color,
                     "display": "standalone"
                 }
             );
 
-            let content = serde_json::to_string(&content).unwrap();
+        let content = serde_json::to_string(&content).unwrap();
 
-            fs::write(web_app_manifest, content).map_err(|err| err.to_string())?;
+        fs::write(web_app_manifest, content).map_err(|err| err.to_string())?;
+    }
+
+    {
+        // browser_config
+        let mut context = Context::new();
+
+        context.insert("path_prefix", path_prefix);
+        context.insert("background_color", &config.windows_background_color);
+
+        let content = tera.render("browser-config", context).map_err(|err| err.to_string())?;
+
+        fs::write(browser_config, content).map_err(|err| err.to_string())?;
+    }
+
+    {
+        // svg_monochrome
+        let mut pgm_config = PGMConfig::new();
+
+        pgm_config.background_color = Some(ColorName::White);
+
+        let mut output = ImageResource::Data(Vec::new());
+
+        to_pgm(&mut output, &mw_input, &pgm_config).map_err(|err| err.to_string())?;
+
+        let pgm_data = output.into_vec().unwrap();
+
+        let threshold_string = format!("{:.2}", config.threshold);
+
+        let rtn = execute_one_stdin(&vec![potrace, "-s", "-k", threshold_string.as_str(), "-", "-o", FILE_SVG_MONOCHROME], output_str, pgm_data)?;
+
+        if rtn != 0 {
+            return Err(format!("Fail to build `{}`.", svg_monochrome.to_string_lossy()));
+        }
+    }
+
+    {
+        // ico
+        let mut ico_config = ICOConfig::new();
+
+        for &size in ICO_SIZE.iter() {
+            ico_config.size.push((size, size));
         }
 
-        {
-            // browser_config
-            let mut context = Context::new();
+        let mut output = ImageResource::from_path(ico);
 
-            context.insert("path_prefix", path_prefix);
-            context.insert("background_color", &config.windows_background_color);
+        to_ico(&mut output, &mw_input, &ico_config).map_err(|err| err.to_string())?;
+    }
 
-            let content = tera.render("browser_config", &context).map_err(|err| err.to_string())?;
-
-            fs::write(browser_config, content).map_err(|err| err.to_string())?;
-        }
-
-        {
-            // svg_monochrome
-            let mut pgm_config = PGMConfig::new();
-
-            pgm_config.background_color = Some(ColorName::White);
-
-            let mut output = ImageResource::Data(Vec::new());
-
-            to_pgm(&mut output, &mw_input, &pgm_config).map_err(|err| err.to_string())?;
-
-            let pgm_data = output.into_vec().unwrap();
-
-            let threshold_string = format!("{:.2}", config.threshold);
-
-            let rtn = execute_one_stdin(&vec![potrace, "-s", "-k", threshold_string.as_str(), "-", "-o", FILE_SVG_MONOCHROME], output_str, pgm_data)?;
-
-            if rtn != 0 {
-                return Err(format!("Fail to build `{}`.", svg_monochrome.to_string_lossy()));
-            }
-        }
-
-        {
-            // ico
-            let mut ico_config = ICOConfig::new();
-
-            for &size in ICO_SIZE.iter() {
-                ico_config.size.push((size, size));
-            }
-
-            let mut output = ImageResource::from_path(ico);
-
-            to_ico(&mut output, &mw_input, &ico_config).map_err(|err| err.to_string())?;
-        }
-
-        {
-            // png_vec
-            for (i, png) in png_vec.iter().enumerate() {
-                let size = PNG_SIZE[i];
-
-                let mut png_config = PNGConfig::new();
-                png_config.shrink_only = false;
-                png_config.width = size;
-                png_config.height = size;
-
-                let mut output = ImageResource::from_path(png);
-
-                to_png(&mut output, &mw_input, &png_config).map_err(|err| err.to_string())?;
-            }
-        }
-
-        {
-            // png_ios_background
-            let mw = mw_input.into_magick_wand().unwrap();
-
-            let mut pw = PixelWand::new();
-            pw.set_color(config.ios_background_color.as_str())?;
-            mw.set_image_background_color(&pw)?;
-            mw.set_image_alpha_channel(bindings::AlphaChannelOption_RemoveAlphaChannel)?;
+    {
+        // png_vec
+        for (i, png) in png_vec.iter().enumerate() {
+            let size = PNG_SIZE[i];
 
             let mut png_config = PNGConfig::new();
             png_config.shrink_only = false;
-            png_config.width = 180;
-            png_config.height = 180;
+            png_config.width = size;
+            png_config.height = size;
 
-            let mw_input = ImageResource::MagickWand(mw);
-
-            let mut output = ImageResource::from_path(png_ios_background);
+            let mut output = ImageResource::from_path(png);
 
             to_png(&mut output, &mw_input, &png_config).map_err(|err| err.to_string())?;
         }
-
-        Ok(0)
-    } else {
-        Ok(0)
     }
 
-//    let output_path = Path::new(&config.output);
-//
-//    let favicon_path = Path::join(&output_path, Path::new("favicon.ico"));
-//
-//    let favicons_path = Path::join(&output_path, Path::new("favicons"));
-//
-//    if let Err(_) = fs::create_dir_all(&favicons_path) {
-//        return Err(String::from("Cannot create favicons folder."));
-//    }
-//
-//    let mut ico_config = image_convert::ICOConfig::new();
-//
-//    ico_config.size.push((48, 48));
-//    ico_config.size.push((32, 32));
-//    ico_config.size.push((16, 16));
-//
-//    let image_source = image_convert::ImageResource::from_path(&config.input);
-//    let mut image_destination = image_convert::ImageResource::from_path(favicon_path);
-//
-//    if let Err(e) = image_convert::to_ico(&mut image_destination, &image_source, &ico_config) {
-//        return Err(e.to_string());
-//    }
-//
-//    let (tx_1, rx) = mpsc::channel();
-//
-//    for &png_size in PNG_SIZE.iter() {
-//        let input = config.input.clone();
-//        let favicons_path = favicons_path.clone();
-//
-//        let tx_2 = tx_1.clone();
-//
-//        thread::spawn(move || {
-//            let file_name = format!("favicon-{}.png", png_size);
-//            let png_path = Path::join(&favicons_path, Path::new(&file_name));
-//            let image_source = image_convert::ImageResource::from_path(&input);
-//            let mut image_destination = image_convert::ImageResource::from_path(png_path);
-//            let mut png_config = image_convert::PNGConfig::new();
-//            png_config.shrink_only = false;
-//            png_config.width = png_size;
-//            png_config.height = png_size;
-//
-//            image_convert::to_png(&mut image_destination, &image_source, &png_config).unwrap();
-//            tx_2.send(0).unwrap();
-//        });
-//    }
-//
-//    let browserconfig_path = Path::join(&favicons_path, Path::new("browser_config.xml"));
-//
-//    fs::write(&browserconfig_path, include_str!("resources/browser_config.xml")).unwrap();
-//
-//    let manifest_path = Path::join(&favicons_path, Path::new("manifest.json"));
-//
-//    fs::write(&manifest_path, include_str!("resources/manifest.json")).unwrap();
-//
-//    let html_path = Path::join(&output_path, Path::new("favicon.html"));
-//
-//    fs::write(&html_path, include_str!("resources/favicon.html")).unwrap();
-//
-//    for _ in PNG_SIZE.iter() {
-//        rx.recv().unwrap();
-//    }
-}
+    {
+        // png_ios_background
+        let mw = mw_input.into_magick_wand().unwrap();
 
-//fn check_exist_single_thread<P: AsRef<P>>(path: P) {
-//    let path = path.as_ref();
-//
-//    if path.exists() {
-//        loop {
-//            println!("Do you want to overwrite `{}`? [Y/N] ", path.canonicalize().map_err(|err| ScannerError::IOError(err))?.to_string_lossy());
-//
-//            io::stdout().flush().map_err(|err| ScannerError::IOError(err))?;
-//
-//            match sc.next()? {
-//                Some(token) => {
-//                    match Boolean::from_string(token) {
-//                        Ok(token) => {
-//                            return Ok(token.get_bool());
-//                        }
-//                        Err(_) => {
-//                            continue;
-//                        }
-//                    }
-//                }
-//                None => {
-//                    return Ok(false);
-//                }
-//            }
-//        }
-//    } else {
-//        Ok(true)
-//    }
-//}
+        let mut pw = PixelWand::new();
+        pw.set_color(config.ios_background_color.as_str())?;
+        mw.set_image_background_color(&pw)?;
+        mw.set_image_alpha_channel(bindings::AlphaChannelOption_RemoveAlphaChannel)?;
+
+        let mut png_config = PNGConfig::new();
+        png_config.shrink_only = false;
+        png_config.width = 180;
+        png_config.height = 180;
+
+        let mw_input = ImageResource::MagickWand(mw);
+
+        let mut output = ImageResource::from_path(png_ios_background);
+
+        to_png(&mut output, &mw_input, &png_config).map_err(|err| err.to_string())?;
+    }
+
+    tera.add_raw_template("html-head", include_str!("resources/favicon.html")).map_err(|err| err.to_string())?;
+
+    let mut context = Context::new();
+
+    context.insert("path_prefix", path_prefix);
+    context.insert("android_background_color", &config.android_background_color);
+    context.insert("windows_background_color", &config.windows_background_color);
+    context.insert("web_app_manifest", FILE_WEB_APP_MANIFEST);
+    context.insert("png_ios_background", FILE_PNG_IOS_BACKGROUND);
+
+    let content = tera.render("html-head", context).map_err(|err| err.to_string())?;
+
+    println!("{}", content);
+
+    Ok(0)
+}
